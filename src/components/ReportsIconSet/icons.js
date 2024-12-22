c49554d59e80284d9b3c36f390a5cf95b7576d0e
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-12-22 14:46:27 +0100

    Add branch.

---
+++ src/components/ReportsIconSet/icons.js
@@ -0,0 +1,18 @@
+export const expencesIcons = [
+    "/icons/sprite.svg#icon-products",
+    "/icons/sprite.svg#icon-coctail",
+    "/icons/sprite.svg#icon-kite",
+    "/icons/sprite.svg#icon-hands-holding-heart",
+    "/icons/sprite.svg#icon-car",
+    "/icons/sprite.svg#icon-couch",
+    "/icons/sprite.svg#icon-tools",
+    "/icons/sprite.svg#icon-invoice",
+    "/icons/sprite.svg#icon-clay",
+    "/icons/sprite.svg#icon-book",
+    "/icons/sprite.svg#icon-ufo",
+]
+
+export const incomeIcons = [
+    "/icons/sprite.svg#icon-salary",
+    "/icons/sprite.svg#icon-add-icome",
+]
