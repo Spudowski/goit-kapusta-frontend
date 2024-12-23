15d7dafe5cfe119bf7aa66155d50ab84826630d8
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-12-23 11:14:15 +0100

    summary

--- src/components/ReportsIncExpSum/ReportsIncExpSum.jsx
+++ src/components/ReportsIncExpSum/ReportsIncExpSum.jsx
@@ -1,21 +1,74 @@
-/** @format */
+import { useState, useEffect } from "react";
 import "../../App.css";
-import { selectIncomesStat, selectExpenseStat } from "../../redux/storeSlice";
+import {
+  selectIncomesStat,
+  selectExpenseStat,
+  selectToken,
+} from "../../redux/storeSlice";
 import css from "./ReportsIncExpSum.module.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { userTransactionPeriodDate } from "../../redux/transaction/operation";
 
 export default function ReportsIncExpSum() {
-  const totalIncomeMonth = useSelector(selectIncomesStat);
-  const januaryIncome = totalIncomeMonth?.January || 0; // Pobierz wartość January lub 0, jeśli undefined
+  const currentMonthIndex = new Date().getMonth();
+  const currentYear = new Date().getFullYear();
+  const [monthIndex, setMonthIndex] = useState(currentMonthIndex);
+  const [year, setYear] = useState(currentYear);
+  const token = useSelector(selectToken);
+  const dispatch = useDispatch();
+
   const totalExpenseMonth = useSelector(selectExpenseStat);
-  const januaryExpense = totalExpenseMonth?.January || 0; // Pobierz wartość January lub 0, jeśli undefined
+  const totalIncomeMonth = useSelector(selectIncomesStat);
+
+  const monthIncome = totalIncomeMonth ? totalIncomeMonth[monthIndex] || 0 : 0;
+  const monthExpense = totalExpenseMonth
+    ? totalExpenseMonth[monthIndex] || 0
+    : 0;
+
+  const handleMonthChange = (newMonthIndex) => {
+    setMonthIndex(newMonthIndex);
+  };
+
+  const handleYearChange = (newYear) => {
+    setYear(newYear);
+  };
+
+  useEffect(() => {
+    dispatch(
+      userTransactionPeriodDate({
+        monthIndex,
+        year,
+        token,
+      })
+    );
+  });
+
   return (
     <div className={css.backgroundRoboczeDoUsuniecia}>
       <div className={css.bar}>
+        <p>Month:</p>
+        <select
+          value={monthIndex}
+          onChange={(e) => handleMonthChange(Number(e.target.value))}
+        >
+          {Array.from({ length: 12 }).map((_, index) => (
+            <option key={index} value={index}>
+              {new Date(0, index).toLocaleString("en", { month: "long" })}
+            </option>
+          ))}
+        </select>
+
+        <p>Year:</p>
+        <input
+          type="number"
+          value={year}
+          onChange={(e) => handleYearChange(Number(e.target.value))}
+        />
+
         <p>Expenses:</p>
-        <p className={css.textExpense}>{`${januaryExpense}`}</p>
+        <p className={css.textExpense}>{`${monthExpense}`}</p>
         <p>Income:</p>
-        <p className={css.textIncome}>{`${januaryIncome}`}</p>
+        <p className={css.textIncome}>{`${monthIncome}`}</p>
       </div>
     </div>
   );
--- src/redux/transaction/operation.js
+++ src/redux/transaction/operation.js
@@ -1,103 +1,128 @@
-import Axios from "axios"
-import { createAsyncThunk } from "@reduxjs/toolkit"
-
+import Axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 const axios = Axios.create({
-    baseURL: 'http://localhost:3000/api'
-  });
-
+  baseURL: "http://localhost:3000/api",
+});
 
 //Add an income
 
 export const addUserIncome = createAsyncThunk(
-    'addUserIncome/fetchAddUserIncome', 
-    async(income, token) => {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`
-        const resp = await axios.post('/transaction/income',
-            //income
-            {
-                "description": "Income description",
-                "amount": 100,
-                "date": "2020-12-31"
-            }
-        )
-        return resp.data
-})
+  "addUserIncome/fetchAddUserIncome",
+  async (income, token) => {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    const resp = await axios.post(
+      "/transaction/income",
+      //income
+      {
+        description: "Income description",
+        amount: 100,
+        date: "2020-12-31",
+      }
+    );
+    return resp.data;
+  }
+);
 
 //Get income stats
 
 export const getUserIncome = createAsyncThunk(
-    'getUserIncome/fetchGetUserIncome', 
-    async(token) => {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`
-        const resp = await axios.get('/transaction/income')
-        return resp.data
-})
-
+  "getUserIncome/fetchGetUserIncome",
+  async (token) => {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    const resp = await axios.get("/transaction/income");
+    return resp.data;
+  }
+);
 
 //Add an expense
 export const addUserExpense = createAsyncThunk(
-    'addUserExpense/fetchAddUserExpense', 
-    async(expense, token) => {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`
-        const resp = await axios.post('/transaction/expense',
-            //expense
-            {
-                "description": "Expense description",
-                "amount": 100,
-                "date": "2020-12-31",
-                "category": "Sellary"
-              }
-        )
-        return resp.data
-})
+  "addUserExpense/fetchAddUserExpense",
+  async (expense, token) => {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    const resp = await axios.post(
+      "/transaction/expense",
+      //expense
+      {
+        description: "Expense description",
+        amount: 100,
+        date: "2020-12-31",
+        category: "Sellary",
+      }
+    );
+    return resp.data;
+  }
+);
 
 //Get expense stats
 export const getUserExpense = createAsyncThunk(
-    'getUserExpense/fetchGetUserExpense', 
-    async(token) => {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`
-        const resp = await axios.get('/transaction/expense')
-        return resp.data
-})
+  "getUserExpense/fetchGetUserExpense",
+  async (token) => {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    const resp = await axios.get("/transaction/expense");
+    return resp.data;
+  }
+);
 
 // Delete transaction
 export const deleteUserExpense = createAsyncThunk(
-    'deleteUserExpense/fetchDeleteUserExpense', 
-    async(transactionId,token) => {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`
-        const resp = await axios.get(`/transaction/${transactionId}`)
-        return resp.data
-})
-
+  "deleteUserExpense/fetchDeleteUserExpense",
+  async (transactionId, token) => {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    const resp = await axios.get(`/transaction/${transactionId}`);
+    return resp.data;
+  }
+);
 
 //Get categories for incomes
 
 export const userIncomeCategory = createAsyncThunk(
-    'userIncomeCategory/fetchUserIncomeCategory', 
-    async(token) => {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`
-        const resp = await axios.get('/transaction/income-categories')
-        return resp.data
-})
-
+  "userIncomeCategory/fetchUserIncomeCategory",
+  async (token) => {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    const resp = await axios.get("/transaction/income-categories");
+    return resp.data;
+  }
+);
 
 //Get categories for expenses
 
 export const userExpenseCategory = createAsyncThunk(
-    'userExpenseCategory/fetchUserExpenseCategory', 
-    async(token) => {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`
-        const resp = await axios.get('/transaction/expense-categories')
-        return resp.data
-})
+  "userExpenseCategory/fetchUserExpenseCategory",
+  async (token) => {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    const resp = await axios.get("/transaction/expense-categories");
+    return resp.data;
+  }
+);
 
 //Get transactions data for a specific period
 
+// export const userTransactionPeriodDate = createAsyncThunk(
+//     'userTransactionPeriodDate/fetchUserTransactionPeriodDate',
+//     async(startDate, endDate, token) => {
+//         axios.defaults.headers.common.Authorization = `Bearer ${token}`
+//         const resp = await axios.get(`/transaction/period-data?startDate=${startDate}&endDate=${endDate}`)
+//         return resp.data
+// })
+
 export const userTransactionPeriodDate = createAsyncThunk(
-    'userTransactionPeriodDate/fetchUserTransactionPeriodDate', 
-    async(startDate, endDate, token) => {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`
-        const resp = await axios.get(`/transaction/period-data?startDate=${startDate}&endDate=${endDate}`)
-        return resp.data
-})
+  "userTransactionPeriodDate/fetchUserTransactionPeriodDate",
+  async ({ monthIndex, year, token }, { rejectWithValue }) => {
+    try {
+      console.log("Dane przekazane do zapytania:", { monthIndex, year });
+      console.log("Token użyty do autoryzacji:", token);
+      axios.defaults.headers.common.Authorization =
+        axios.defaults.headers.common.Authorization = `Bearer ${encodeURIComponent(
+          token
+        )}`;
+      const resp = await axios.get(
+        `/transaction/period-data?monthIndex=${monthIndex}&year=${year}`
+      );
+      return resp.data;
+    } catch (error) {
+      console.error("Błąd podczas pobierania danych:", error);
+      return rejectWithValue(error.response?.data || "Błąd serwera");
+    }
+  }
+);
